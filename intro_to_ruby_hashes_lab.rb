f1c478ede5b9b2488a99fcce08ab86eabd9c6538
Dasha Lary <dk2800@nyu.edu>
2020-09-11 19:04:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,8 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-		monopoly = {:railroads => {:pieces => 4},
-		  :names => {}
+		monopoly = {:railroads => {:pieces => 4, :names => {}},
+	
 		}
 
 end
